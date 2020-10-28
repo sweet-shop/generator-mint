5fdcc5c6fe43dc14c00e312df4ed5e6efa6a00af
huarxia <liubiao0810@live.cn>
2020-10-28 16:21:09 +0800

    feat: 🍳 新增components示例文件

---
+++ generators/app/templates/src/components/demo/index.vue
@@ -0,0 +1,16 @@
+<template src="./index.tpl.html"></template>
+<script>
+export default {
+    name: 'demo',
+    components: {},
+    props: '',
+    data() {
+        return {};
+    },
+    computed: {},
+    created() {},
+    mounted() {},
+    methods: {}
+};
+</script>
+<style src="./index.less" lang="less" scoped></style>
